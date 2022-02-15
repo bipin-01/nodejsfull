85c900863e56afda324346f741b73c4280510cf2
bipin-01 <poudel7700@gmail.com>
2022-02-15 22:42:05 +0545

    A bit of touch to validator npm package

---
+++ playgroung/vaidator/validator.js
@@ -0,0 +1,10 @@
+let validator = require('validator');
+
+const verifyEmail = validator.isEmail('foo@bar.com');
+console.log(verifyEmail)
+
+const parseData = "Hellothisisparsedata123";
+
+const checkAlphaNumberic = validator.isAlphanumeric(parseData)
+
+console.log(checkAlphaNumberic);
