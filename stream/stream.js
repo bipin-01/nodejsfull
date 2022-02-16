a70622131b75bbd7c5943e632d84704bb97a9433
bipin-01 <poudel7700@gmail.com>
2022-02-16 12:02:25 +0545

    Learned pipe streaming concept rather than normal streaming

--- stream/stream.js
+++ stream/stream.js
@@ -5,20 +5,26 @@ const server = http.createServer();
 
 server.on('request', (req, res)=>{
 
-    const rstream = fs.createReadStream("input.txt");       //change the file name to inputss.txt and check the output
+    // const rstream = fs.createReadStream("input.txt");       //change the file name to inputss.txt and check the output
 
-    rstream.on("data", (chunkData) => {
-        res.write(chunkData)
-    });
+    // rstream.on("data", (chunkData) => {
+    //     res.write(chunkData)
+    // });
 
-    rstream.on("end", () => {
-        res.end();
-    } )
+    // rstream.on("end", () => {
+    //     res.end();
+    // } )
 
-    rstream.on("error", (err) => {
-        console.log(err);
-        res.end(`${err}`);
-    })
+    // rstream.on("error", (err) => {
+    //     console.log(err);
+    //     res.end(`${err}`);
+    // })
+
+    const rstream = fs.createReadStream('input.txt');
+    rstream.pipe(res);
+
+    const rstream2 = fs.createReadStream('input1.txt');
+    rstream2.pipe(res);
 });
 
 server.listen(8000, 
