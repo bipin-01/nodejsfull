60a68967fc58f7203456171e239e5c78c0669bfd
bipin-01 <poudel7700@gmail.com>
2022-02-16 11:54:11 +0545

    move file to root directory

--- httpserver/index.js
+++ httpserver/index.js
@@ -9,12 +9,27 @@
 // we should include an HTTP headeer with the correct content type
 
 const http = require("http");
+const fs = require("fs")
+const process = require("process")
+
+const data = fs.readFileSync(`${__dirname}/dataJson.json`, "utf-8")
+const jsData = JSON.parse(data);
+console.log(jsData)
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the other side")
+  if (req.url === "./") res.end("Hello from the other side");
+  else if (req.url === "/about") res.end("Hello from the About Us side");
+  else if (req.url === "/userapi") {
+    res.writeHead(200, {"Content-type":"application/json"})
+    res.end(data)
+    }
+  
+  else{
+      res.writeHead(404, {"Content-Type":"text/html"})
+      res.end(`<h2>404 Error page does not exists</h2>`)
+  }
 });
 
 server.listen(8000, () => {
-    console.log("Listening to the port no: 8000")
+  console.log("Listening to the port no: 8000");
 });
-
